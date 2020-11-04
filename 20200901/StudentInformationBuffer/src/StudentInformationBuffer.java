7f8c57a521631a2e820590f1624e299095a2106a
百川 <55468046+zgBCQ@users.noreply.github.com>
2020-11-04 16:55:45 +0800

    学生信息缓冲流

--- 20200901/StudentInformationBuffer/src/StudentInformationBuffer.java
+++ 20200901/StudentInformationBuffer/src/StudentInformationBuffer.java
@@ -1,14 +1,18 @@
+/*******************************************
+将学生信息存入文件
+ 使用了图形界面，但是并没有去考虑某个对话框如果没有数据录入时的情况
+ 第一个是录入学生信息，需要用户自行在输入框中输入各种信息，而且年龄和成绩只能输入数字（输入其他符号无反应）
+ 输入完一个学生信息点击“将该学生的信息写入文件的位置”按钮会弹出对话框选择保存的位置（只允许选择目录）学生信息会追加在该目录下的StudentInformation.txt文件中
+ 再次点击该按钮，不会再选择保存位置
+ 第二个显示一个txt文件的信息，没有去考虑txt文件中录入的不是学生信息这种情况，会将txt文件中所有信息输出
+ ******************************************/
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.*;
 
-public class StudentInformationBuffer {
-}
-class StudentInformationBufferGui{
+public class StudentInformationBuffer{
     JFrame jFrame;
     JPanel jPanelFunction;
     JButton jButtonInput;
@@ -22,49 +26,43 @@ class StudentInformationBufferGui{
     JPanel jPanelGrade;
     JTextField jTextFieldGradeInt;
     JTextField jTextFieldGradeDouble;
+    JButton jButtonGo;
+    JLabel jLabelFileLocation;
     JPanel jPanelOutput;
     JTextArea jTextAreaStudent;
     File file;
     String Location = null;
-    StudentInformationBufferGui(){
+    StudentInformationBuffer(){
         jFrame = new JFrame("学生信息");
         jFrame.setLayout(new BorderLayout());
-        (jButtonInput = new JButton("向文件中写数据")).addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cardLayout.show(jPanelWorkSpace,"Input");
-                JFileChooser jFileChooser = new JFileChooser();
-                if(jFileChooser.showSaveDialog(jFrame) == JFileChooser.APPROVE_OPTION){
-                    file = jFileChooser.getSelectedFile();
-                    Location = file.getAbsolutePath();
-                }
-            }
-        });
-        (jButtonOutput = new JButton("从文件中读取数据")).addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cardLayout.show(jPanelWorkSpace,"Output");
-                JFileChooser jFileChooser = new JFileChooser();
-                if(jFileChooser.showOpenDialog(jFrame) == JFileChooser.APPROVE_OPTION){
-                    file = jFileChooser.getSelectedFile();
-                    Location = file.getAbsolutePath();
-                    Reader reader = null;
-                    try {
-                        reader = new FileReader(file);
-                    } catch (FileNotFoundException fileNotFoundException) {
-                        fileNotFoundException.printStackTrace();
-                    }
+        (jButtonInput = new JButton("向文件中写数据")).addActionListener(e -> cardLayout.show(jPanelWorkSpace,"Input"));
+        (jButtonOutput = new JButton("从文件中读取数据")).addActionListener(e -> {
+            cardLayout.show(jPanelWorkSpace,"Output");
+            JFileChooser jFileChooser = new JFileChooser();
+            if(jFileChooser.showOpenDialog(jFrame) == JFileChooser.APPROVE_OPTION){
+                file = jFileChooser.getSelectedFile();
+                Location = file.getAbsolutePath();
+                Reader reader;
+                try {
+                    reader = new FileReader(file);
                     BufferedReader bufferedReader = new BufferedReader(reader);
                     String s;
                     while (true) {
                         try {
-                            if (((s = bufferedReader.readLine())!=null)) {
-                                jTextAreaStudent.append(s + "\r\n");
+                            if ((s = bufferedReader.readLine())!=null) {
+                                jTextAreaStudent.append(s + System.lineSeparator());                                    //缓冲流读取
+                            }
+                            else{
+                                bufferedReader.close();
+                                reader.close();
+                                break;
                             }
                         } catch (IOException ioException) {
                             ioException.printStackTrace();
                         }
                     }
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
                 }
             }
         });
@@ -73,7 +71,7 @@ class StudentInformationBufferGui{
         jPanelFunction.add(jButtonOutput);
         jFrame.add(jPanelFunction,BorderLayout.NORTH);
         (jPanelWorkSpace = new JPanel()).setLayout(cardLayout = new CardLayout());
-        (jPanelInput = new JPanel()).setLayout(new GridLayout(4,2));
+        (jPanelInput = new JPanel()).setLayout(new GridLayout(5,2));
         jPanelInput.add(new JLabel("学生姓名"));
         jPanelInput.add(jTextFieldName = new JTextField());
         jPanelInput.add(new JLabel("院系名称"));
@@ -89,6 +87,20 @@ class StudentInformationBufferGui{
         (jTextFieldGradeDouble =new JTextField()).addKeyListener(new KeyInt());
         jPanelGrade.add(jTextFieldGradeDouble);
         jPanelInput.add(jPanelGrade);
+        (jButtonGo = new JButton("将该学生信息写入文件的位置")).addActionListener(e -> {
+            if(Location == null) {
+                JFileChooser jFileChooser = new JFileChooser();
+                jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                if (jFileChooser.showSaveDialog(jFrame) == JFileChooser.APPROVE_OPTION) {
+                    Location = jFileChooser.getSelectedFile().getAbsolutePath();
+                    jLabelFileLocation.setText(Location);
+                }
+            }
+            Student student = new Student(jTextFieldName.getText(),jTextFieldCollage.getText(),jTextFieldYear.getText(),jTextFieldGradeInt.getText(),jTextFieldGradeDouble.getText());
+            student.studentWrite(Location);
+        });
+        jPanelInput.add(jButtonGo);
+        jPanelInput.add(jLabelFileLocation = new JLabel());
         jPanelWorkSpace.add(jPanelInput,"Input");
         (jPanelOutput = new JPanel()).setLayout(new BorderLayout());
         jTextAreaStudent = new JTextArea();
@@ -101,7 +113,7 @@ class StudentInformationBufferGui{
         jFrame.setDefaultCloseOperation(3);
         jFrame.setVisible(true);
     }
-    class KeyInt extends KeyAdapter{
+    static class KeyInt extends KeyAdapter{
         @Override
         public void keyTyped(KeyEvent e) {
             char KeyChar = e.getKeyChar();
@@ -111,7 +123,31 @@ class StudentInformationBufferGui{
         }
     }
     public static void main(String [] args){
-        new StudentInformationBufferGui();
+        new StudentInformationBuffer();
     }
 }
-
+class Student{
+    String Name;
+    String Collage;
+    int Year;
+    double Grade;
+    Student(String name,String collage,String year,String gradeInt,String gradeDouble){
+        Name = name;
+        Collage = collage;
+        Year = Integer.parseInt(year);
+        Grade = Integer.parseInt(gradeInt) * 1.0 + Integer.parseInt(gradeDouble) * 1.0 / (Math.pow(10.0,gradeDouble.length()));
+    }
+    void studentWrite(String Location){
+        try {
+            File file = new File(Location + "/StudentInformation" + ".txt");
+            FileWriter writer = new FileWriter(file,true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.newLine();
+            bufferedWriter.write(Name + " " + Collage + " " + Year + " " + Grade);                                  //缓冲流写入
+            bufferedWriter.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
