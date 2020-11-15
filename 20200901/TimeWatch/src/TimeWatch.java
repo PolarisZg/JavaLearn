c06fdb2c6c59b56d1ec93f1e5590dac4079e407d
百川 <55468046+zgBCQ@users.noreply.github.com>
2020-11-15 16:51:11 +0800

    时钟程序

--- 20200901/TimeWatch/src/TimeWatch.java
+++ 20200901/TimeWatch/src/TimeWatch.java
@@ -1,11 +1,30 @@
 import javax.swing.*;
+import java.time.LocalTime;
 
-public class TimeWatch extends Thread{
+public class TimeWatch {
     JFrame jFrame;
     JLabel jLabelWatch;
-
-    @Override
-    public void run() {
-        
+    TimeWatch(){
+        jFrame = new JFrame("Watch");
+        jLabelWatch = new JLabel();
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            while (true) {
+                LocalTime localTime = LocalTime.now();
+                jLabelWatch.setText(localTime.getHour() + " " + localTime.getMinute() + " " + localTime.getSecond());
+            }
+        });
+        thread.start();
+        jFrame.add(jLabelWatch);
+        jFrame.setSize(200,200);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+    }
+    public static void main(String [] args){
+        new TimeWatch();
     }
 }
