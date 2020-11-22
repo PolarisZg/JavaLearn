0ee0df307e8f54354ef191aa470a77b5a77f781b
百川 <55468046+zgBCQ@users.noreply.github.com>
2020-11-22 16:10:29 +0800

    Del

--- 20200901/ChatClientWithGui/src/ChatClientWithGui.java
+++ 20200901/ChatClientWithGui/src/ChatClientWithGui.java
@@ -1,31 +1,31 @@
-//需要先启动服务器程序( 此程序 ) ， 再启动客户端
+// 需要先启动服务器程序，再启动客户端( 此程序 )
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.net.InetAddress;
 import java.net.Socket;
 
-public class ChatServer {
+
+public class ChatClientWithGui {
     public static void main(String[] args) {
-        new ChatServerGui();
+        new ChatClientWithGui_Gui();
     }
 }
 
-class ChatServerGui {
+class ChatClientWithGui_Gui {
     JFrame jFrame;
     JTextArea jTextAreaScreen;
     JTextArea jTextAreaInput;
     JButton jButtonSend;
     JPanel jPanelInput;
 
-    ServerSocket serverSocket;
     Socket socket;
     private static final int SERVER_PORT = 9001;
 
-    ChatServerGui() {
-        jFrame = new JFrame("聊天_服务器");
+    ChatClientWithGui_Gui() {
+        jFrame = new JFrame("聊天_客户端");
         jFrame.add(new JScrollPane(jTextAreaScreen = new JTextArea()));
         jTextAreaScreen.addKeyListener(new KeyAdapter() {
             @Override
@@ -40,7 +40,7 @@ class ChatServerGui {
         jPanelInput.add(new JScrollPane(jTextAreaInput = new JTextArea()));
         jPanelInput.add(jButtonSend = new JButton("发送"), BorderLayout.EAST);
         jButtonSend.addActionListener(e -> {
-            jTextAreaScreen.append("服务器 : -->" + jTextAreaInput.getText() + "\r\n");
+            jTextAreaScreen.append("客户端 : -->" + jTextAreaInput.getText() + "\r\n");
             try {
                 socket.getOutputStream().write(jTextAreaInput.getText().getBytes());
             } catch (IOException ioException) {
@@ -56,14 +56,13 @@ class ChatServerGui {
         jFrame.setVisible(true);
 
         try {
-            serverSocket = new ServerSocket(SERVER_PORT);
-            jTextAreaScreen.append("服务器已启动，等待客户端" + "\r\n");
-            socket = serverSocket.accept();
-            jTextAreaScreen.append("已连接" + "\r\n");
+            InetAddress inetAddress = InetAddress.getByName("localhost");
+            socket = new Socket(inetAddress, SERVER_PORT);
+            jTextAreaInput.append("客户端已启动" + "\r\n");
             byte[] b = new byte[1024];
             int length;
-            while ((length = socket.getInputStream().read(b)) != -1){
-                jTextAreaScreen.append("客户端 : -->" + new String(b,0,length) + "\r\n");
+            while ((length = socket.getInputStream().read(b)) != -1) {
+                jTextAreaScreen.append("服务器 : -->" + new String(b, 0, length) + "\r\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
