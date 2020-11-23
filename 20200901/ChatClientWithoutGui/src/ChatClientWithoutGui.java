fe94685cd6e9f7aeb08d2c2378a6465e759a1fb4
百川 <55468046+zgBCQ@users.noreply.github.com>
2020-11-23 13:58:27 +0800

    聊天程序_客户端_无图形界面

--- 20200901/ChatClientWithoutGui/src/ChatClientWithoutGui.java
+++ 20200901/ChatClientWithoutGui/src/ChatClientWithoutGui.java
@@ -6,6 +6,7 @@
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ChatClientWithoutGui {
 
@@ -17,13 +18,36 @@ public class ChatClientWithoutGui {
             InetAddress inetAddress = InetAddress.getByName("localhost");
             socket = new Socket(inetAddress, SERVER_PORT);
             System.out.println("客户端已启动");
-            byte[] b = new byte[1024];
-            int length;
-            length = socket.getInputStream().read(b);
-            System.out.println("服务器 : -->" + new String(b, 0, length));
-            socket.getOutputStream().write("这是来自客户端的信息".getBytes());
+            System.out.println("输入文字 , 按回车发送");
+            Thread threadIn = new Thread(() -> {
+                byte[] b = new byte[1024];
+                int length = 0;
+                while (true) {
+                    try {
+                        if ((length = socket.getInputStream().read(b)) == -1) break;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.out.println("服务器停止运行 , 请先启动服务器 \n 本程序已停止");
+                        System.exit(0);
+                    }
+                    System.out.println("服务器 : -->" + new String(b, 0, length));
+                }
+            });
+            Thread threadOut = new Thread(() -> {
+                while(true){
+                    try {
+                        socket.getOutputStream().write(new Scanner(System.in).nextLine().getBytes());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            threadOut.start();
+            threadIn.start();
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println("服务器停止运行 , 请先启动服务器 \n 本程序已停止");
+            System.exit(0);
         }
     }
 
