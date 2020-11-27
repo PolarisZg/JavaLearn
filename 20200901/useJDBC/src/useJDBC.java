9c857af4ee4c1c643a1ce253320e1958624d8228
百川 <55468046+zgBCQ@users.noreply.github.com>
2020-11-27 12:49:37 +0800

    数据库练习

--- 20200901/useJDBC/src/useJDBC.java
+++ 20200901/useJDBC/src/useJDBC.java
@@ -1,18 +1,38 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class useJDBC {
     
     useJDBC(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/student?serverTimezone=Asia/Shanghai";
-            Connection connection = DriverManager.getConnection(url,"PolarisZg","8896859ab");
+            System.out.println("输入数据库名:");
+            String schemaName = new Scanner(System.in).nextLine();
+            String url = "jdbc:mysql://localhost:3306/" + schemaName + "?serverTimezone=Asia/Shanghai";
+            System.out.println("输入用户名:");
+            String user = new Scanner(System.in).nextLine();
+            System.out.println("输入密码:");
+            String password = new Scanner(System.in).nextLine();
+            Connection connection = DriverManager.getConnection(url,user,password);
             Statement statement = connection.createStatement();
-            statement.addBatch();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM `学生成绩表`;");
+            statement.execute("DROP TABLE IF EXISTS 学生表;" );
+            statement.execute("CREATE TABLE 学生表 ( 姓名 VARCHAR (10) PRIMARY KEY NOT NULL," +
+                    "性别 VARCHAR (2) NULL," +
+                    "院系 VARCHAR(10) NULL," +
+                    "出生日期 DATE NULL," +
+                    "平均成绩 DOUBLE NULL );");
+            statement.execute("INSERT INTO 学生表 VALUES ('张蔷' , '男' , '控计学院' , '2001-05-01' , '80.2');");
+            statement.execute("INSERT INTO 学生表 VALUES ('田宇' , '男' , '电力学院' , '2001-06-01' , '82.4');");
+            statement.execute("INSERT INTO 学生表 VALUES ('张良' , '男' , '控计学院' , '2002-05-01' , '40.2');");
+            statement.execute("INSERT INTO 学生表 VALUES ('李红' , '女' , '控计学院' , '2002-05-01' , '84.6');");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM 学生表 WHERE 性别 = '男';");
             while(resultSet.next()){
-                System.out.println(resultSet.getString(2));
+                for(int i = 1 ; i <= 5 ; i++ )
+                    System.out.print(resultSet.getString(i) + "    ");
+                System.out.println();
             }
+            statement.execute("UPDATE 学生表 SET 平均成绩 = 平均成绩 * 0.7 + 30 WHERE 院系 = '控计学院';");
+            statement.execute("DELETE  FROM 学生表 WHERE 平均成绩 <= 60;");
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
